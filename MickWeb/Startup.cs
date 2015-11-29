26d52bc8218ba052668258298ca8696d251a2dcb
CasperDopestGhost <mikemeyer79@hotmail.com>
2015-11-29 16:45:15 -0600

    Initial commit

---
+++ MickWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MickWeb.Startup))]
+namespace MickWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
